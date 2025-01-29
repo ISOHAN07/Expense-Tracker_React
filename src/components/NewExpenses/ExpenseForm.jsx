06df82381022429e7af134fabbf1db51d9df2047
Sohan Ghosh <isohan2007@gmail.com>
2025-01-29 23:03:57 +0530

    add submit handler and two-way binding

--- src/components/NewExpenses/ExpenseForm.jsx
+++ src/components/NewExpenses/ExpenseForm.jsx
@@ -3,9 +3,9 @@ import './ExpenseForm.css';
 
 export default function ExpenseForm(){
     // Approach having three different state change
-    const [title, setTitle] = useState('');
-    const [amt, setAmt] = useState('');
-    const [date, setDate] = useState('');
+    const [enteredTitle, setTitle] = useState('');
+    const [enteredAmt, setAmt] = useState('');
+    const [enteredDate, setDate] = useState('');
 
     // Can also be done using only one state change hook
     // the change function and the state will comprise of object containing all the change parameters
@@ -33,20 +33,42 @@ export default function ExpenseForm(){
         setDate(event.target.value);
     };
 
+    // for updating the state based on the previous state:
+    // setUserInput((prevState) => {
+    //     return {
+    //         ...prevState,
+    //         other key that needs to be overwritten
+    //     }
+    // });
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const expenseData = {
+            title: enteredTitle,
+            amt: enteredAmt,
+            date: new Date(enteredDate)
+        };
+
+        setAmt('');
+        setDate('');
+        setTitle('');
+    };
+
     return(
-        <form>
+        <form onSubmit={submitHandler}>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
-                    <input type="text" onChange={titleChangeHandler}/>
+                    <input type="text" value = {enteredTitle} onChange={titleChangeHandler}/>
                 </div>
                 <div className='new-expense__control'>
                     <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01" onChange={amtChangeHandler}/>
+                    <input type="number" min="0.01" step="0.01" value={enteredAmt} onChange={amtChangeHandler}/>
                 </div>
                 <div className='new-expense__control'>
                     <label>Date</label>
-                    <input type="date" min="2023-07-21" max="2027-07-20" onChange={dateChangeHandler}/>
+                    <input type="date" min="2023-07-21" max="2027-07-20" value={enteredDate} onChange={dateChangeHandler}/>
                 </div>
             </div>
             <div className='new-expense__actions'>
