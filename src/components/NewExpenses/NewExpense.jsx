62e56d4618fbdec3def391869a4f503d7e073711
Sohan Ghosh <isohan2007@gmail.com>
2025-01-30 22:30:18 +0530

    add cancel button functionality

--- src/components/NewExpenses/NewExpense.jsx
+++ src/components/NewExpenses/NewExpense.jsx
@@ -1,7 +1,10 @@
 import './NewExpense.css';
 import ExpenseForm from './ExpenseForm';
+import { useState } from 'react';
 
 export default function NewExpense(props){
+    const [isEditing, setIsEditing] = useState(false);
+
     const saveExpenseHandler = (enteredExpense) => {
         const expenseData = {
             ...enteredExpense,
@@ -11,9 +14,25 @@ export default function NewExpense(props){
         // lifting the state up to the common ancestor for either use by other child or store there
         props.onAddExpense(expenseData);
         // console.log(expenseData);
+        setIsEditing(false);
     };
 
-    return <div className='new-expense'>
-        <ExpenseForm onSaveExpense = {saveExpenseHandler}/>
-    </div>
-}
+    const startHandler = () => {
+        setIsEditing(true);
+    };
+
+    const stopHandler = () => {
+        setIsEditing(false);
+    };
+
+    return(
+        <div className='new-expense'>
+            {!isEditing && (
+                <button onClick={startHandler}>Add New Expense</button>
+            )}
+            {isEditing && (
+                <ExpenseForm onSaveExpense = {saveExpenseHandler} onCancel = {stopHandler}/>
+            )}
+        </div>
+    );
+};
